0a20be78d7f0cd10f791202c17220dd4d6bde7d1
OXIDE <oxidiu777@gmail.com>
2023-07-26 11:57:22 +0300

    finished

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react'
 
-const Post = ({
+type postProps = {
+  id: number;
+  text: string;
+  handleUpdate: (id: number, text: string) => void;
+  handleDelete: (id: number) => void;
+}
+
+const Post: React.FC<postProps> = ({
   id,
   text,
   handleUpdate,
   handleDelete
 }) => {
-  const [currentText, setCurrentText] = useState(text)
+  const [currentText, setCurrentText] = useState<string>(text)
   return (
     <div >
       <input className={id % 2 === 0 ? 'red' : 'blue'} type="text" value={currentText} onChange={(ev) => setCurrentText(ev.target.value)} />
